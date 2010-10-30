9252c58b461eb0d0d48bdee3f947e3b36d690e70
Paul Schorfheide <pschorf@vpn3-145125.near.uiuc.edu>
2010-10-30 12:34:41 -0500

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Happypets::Application.config.secret_token = 'a21f85fd0f54fd9e33f9dc911f63c4dcda201be594b4b5b2cf0b7d51d080b04f9530e3c3695951efc1037623a8313f32a547fe4cf59697737c832f5802d4dc3d'
